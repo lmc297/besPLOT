b2a6e093579249bf7d64562d589317b7996ab308
Laura Carroll <lmc297@cornell.edu>
2018-07-29 18:16:58 +0200

    Update app.R

--- archive/testing/3colors/app.R
+++ archive/testing/3colors/app.R
@@ -152,8 +152,8 @@ server <- function(input, output) {
         geom_polygon(data=hull.data,aes(x=NMDS1,y=NMDS2,group=metadata,colour=metadata,fill=metadata),alpha=0.30)+
         #geom_text_repel(data=species.scores,aes(x=NMDS1,y=NMDS2,label=species),alpha=0.5,size=3) +  
         geom_point(data=mymds.scores,aes(x=NMDS1,y=NMDS2,colour=metadata,shape="a"),size=2) +
-        scale_color_manual(c("cornflowerblue", "springgreen3", "deeppink3")) + 
-        scale_fill_manual(c("cornflowerblue", "springgreen3", "deeppink3")) +         
+        scale_color_manual(values = c("cornflowerblue", "springgreen3", "deeppink3")) + 
+        scale_fill_manual(values = c("cornflowerblue", "springgreen3", "deeppink3")) +         
         guides(shape=FALSE) +
         coord_cartesian(xlim = ranges$x, ylim = ranges$y)
       nmds.final<-mymds.scores[,1:2]
